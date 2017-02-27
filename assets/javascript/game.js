4727bfa9e34d707c41843666417906526f90d623
rebecca palmore <rpalmore@wttw.com>
2017-02-27 09:45:50 -0600

    updating

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,10 @@
 /*Assign variables for start of game. We will need:
 1. Win counter
 2. Loss counter
-3. Total score counter
+3. User score counter
 4. Random winning number, between 19-120
 5. Random number for crystals 1-4, between 1-12
+6. Total score (adds all user scores together) PUT THIS ON ICE
 
 Write all of the above to HTML
 */
@@ -17,11 +18,19 @@ var crystalB = [];
 var crystalC = [];
 var crystalD = [];
 
-//Generate random winning number as function
+// Start game function
+function startGame () {
+pickWinningNum();
+pickCrystals();
+userScore = 0;
+document.getElementById("user-score").innerHTML = userScore;
+}
 
+startGame ();
+
+//Generate random winning number as function
 function pickWinningNum() {
     winningNum = Math.floor(Math.random() * (120 - 19 +1)) + 19;
-
     console.log("this is the winning number: " + winningNum);
     document.getElementById("winning-num").innerHTML = winningNum;
 };
@@ -37,26 +46,56 @@ function pickCrystals() {
     	+ [crystalB] + (" ") + [crystalC] + (" ") + [crystalD] + (" "));
 };
 
-
-// Start game
-pickWinningNum();
-pickCrystals();
-
-// Write click functions for each crystal
-$("img").click(function() {
-	alert("You clicked a crystal!");
-	if (crystalA) {
-		userScore = [crystalA]};
-	if (crystalB) {
-		userScore = [crystalB]};
-	document.getElementById("user-score").innerHTML = userScore;
+// Write click functions for each crystal and print score
+$("#crystal-a").click(function() {
+	document.getElementById("user-score").innerHTML = userScore += crystalA;
+	if (userScore === winningNum) {
+		winCounter++;
+		document.getElementById("win-counter").innerHTML = winCounter;
+		startGame();
+	} else if (userScore > winningNum) {
+		lossCounter++;
+		document.getElementById("loss-counter").innerHTML = lossCounter;
+		startGame();
+	}
 });
 
-//We can increment up by any value by writing i += x, 
-//where x is how much we want to increment up by. e.g., i += 3 counts up by 3s.
+$("#crystal-b").click(function() {
+	document.getElementById("user-score").innerHTML = userScore += crystalB;
+	if (userScore === winningNum) {
+		winCounter++;
+		document.getElementById("win-counter").innerHTML = winCounter;
+		startGame();
+	} else if (userScore > winningNum) {
+		lossCounter++;
+		document.getElementById("loss-counter").innerHTML = lossCounter;
+		startGame();
+	}
+});
 
-// what is +=???
-// It is a Javascript assignment operator
-// Operator: +=
-// Example: x += y	
-// Same as: x = x + y
+$("#crystal-c").click(function() {
+	document.getElementById("user-score").innerHTML = userScore += crystalC;
+	if (userScore === winningNum) {
+		winCounter++;
+		document.getElementById("win-counter").innerHTML = winCounter;
+		startGame();
+	} else if (userScore > winningNum) {
+		lossCounter++;
+		document.getElementById("loss-counter").innerHTML = lossCounter;
+		startGame();
+	}
+});
+
+$("#crystal-d").click(function() {
+	document.getElementById("user-score").innerHTML = userScore += crystalD;
+	if (userScore === winningNum) {
+		winCounter++;
+		document.getElementById("win-counter").innerHTML = winCounter;
+		startGame();
+	} else if (userScore > winningNum) {
+		lossCounter++;
+		document.getElementById("loss-counter").innerHTML = lossCounter;
+		startGame();
+	}
+});
+
